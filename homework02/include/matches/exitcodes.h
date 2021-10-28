e65b34ffcedd24d4d0f602aed6dd5b3295b666e4
rflban <nabiev.alsn@yandex.ru>
2021-10-28 17:24:11 +0300

    hw02: refactor serial code

--- homework02/include/matches/exitcodes.h
+++ homework02/include/matches/exitcodes.h
@@ -1,6 +1,8 @@
 #ifndef HW02_MATCHES_EXITCODES_H_
 #define HW02_MATCHES_EXITCODES_H_
 
+#include "collections/exitcodes.h"
+
 #define EXIT_SUCCESS_ 0
 #define EXIT_BAD_ALLOC_ 1
 #define EXIT_INPUT_ERROR_ 2
@@ -8,5 +10,18 @@
 #define EXIT_OPENDIR_ERROR_ 4
 #define EXIT_OPENFILE_ERROR_ 5
 #define EXIT_3RDPTY_ERROR_ 6
+
+static inline int convert_exitcode_from_collections(int exitcode) {
+    switch (exitcode) {
+        case PHWC_EXIT_SUCCESS:
+            return EXIT_SUCCESS_;
+        case PHWC_EXIT_NULL_ARG:
+            return EXIT_UNEXPECTED_NULL_ARG_;
+        case PHWC_EXIT_BAD_ALLOC:
+            return EXIT_BAD_ALLOC_;
+        default:
+            return EXIT_3RDPTY_ERROR_;
+    }
+}
 
 #endif  // HW02_MATCHES_EXITCODES_H_
--- homework02/src/serial/scan_dir.c
+++ homework02/src/serial/scan_dir.c
@@ -28,6 +28,70 @@ static size_t find_worst(const vector_of_token_matches_t* matches) {
     return idx;
 }
 
+static int swap_with_worst(vector_of_token_matches_t* best_matches,
+                           size_t token_count, const char* current_filename) {
+    assert(best_matches && current_filename && "NULL args are not allowed");
+
+    size_t swap_pos = find_worst(best_matches);
+
+    if (best_matches->buffer[swap_pos].count < token_count) {
+        string_t fname;
+        int exitcode = convert_exitcode_from_collections(
+            string_init(&fname, current_filename)
+        );
+        if (exitcode != EXIT_SUCCESS_) {
+            return exitcode;
+        }
+        token_matches_t match = { fname, token_count };
+        string_deinit(&(best_matches->buffer[swap_pos].filename));
+        best_matches->buffer[swap_pos] = match;
+    }
+
+    return EXIT_SUCCESS_;
+}
+
+static int scan_file(const char* dent_name, const char* token,
+                     vector_of_token_matches_t* best_matches, size_t* best_of) {
+    assert(dent_name && token && best_matches && "NULL args are not allowed");
+
+    int exitcode = EXIT_SUCCESS_;
+    size_t token_count;
+    FILE* stream = NULL;
+
+    if (!(stream = fopen(dent_name, "r"))) {
+        return EXIT_OPENFILE_ERROR_;
+    }
+
+    exitcode = counttoken_file(stream, token, &token_count);
+    fclose(stream);
+    if (exitcode != EXIT_SUCCESS_) {
+        return exitcode;
+    }
+
+    if (best_matches->size < *best_of) {
+        string_t fname;
+        exitcode = convert_exitcode_from_collections(
+            string_init(&fname, dent_name)
+        );
+        if (exitcode != EXIT_SUCCESS_) {
+            return exitcode;
+        }
+        token_matches_t match = { fname, token_count };
+
+        exitcode = convert_exitcode_from_collections(
+            vector_of_token_matches_t_add(best_matches, match)
+        );
+        if (exitcode != EXIT_SUCCESS_) {
+            string_deinit(&fname);
+            return exitcode;
+        }
+    } else {
+        exitcode = swap_with_worst(best_matches, token_count, dent_name);
+    }
+
+    return exitcode;
+}
+
 int scan_dir(const char* dirpath, const char* token,
              struct token_matches* matches, size_t* best_of) {
     if (!dirpath || !token || !matches || !best_of) {
@@ -47,45 +111,8 @@ int scan_dir(const char* dirpath, const char* token,
     FILE* stream = NULL;
     size_t token_count = 0;
     struct dirent* dent = NULL;
-    while ((dent = readdir(dir))) {
-        if (!(stream = fopen(dent->d_name, "r"))) {
-            exitcode = EXIT_OPENFILE_ERROR_;
-            break;
-        }
-        exitcode = counttoken_file(stream, token, &token_count);
-        fclose(stream);
-        if (exitcode != EXIT_SUCCESS_) {
-            break;
-        }
-
-        if (best_matches.size < *best_of) {
-            string_t fname;
-            if (PHWC_EXIT_SUCCESS != string_init(&fname, dent->d_name)) {
-                exitcode = EXIT_3RDPTY_ERROR_;
-                break;
-            }
-            token_matches_t match = { fname, token_count };
-
-            if (PHWC_EXIT_SUCCESS != 
-                    vector_of_token_matches_t_add(&best_matches, match)) {
-                exitcode = EXIT_3RDPTY_ERROR_;
-                string_deinit(&fname);
-                break;
-            }
-        } else {
-            size_t swap_pos = find_worst(&best_matches);
-
-            if (best_matches.buffer[swap_pos].count < token_count) {
-                string_t fname;
-                if (PHWC_EXIT_SUCCESS != string_init(&fname, dent->d_name)) {
-                    exitcode = EXIT_3RDPTY_ERROR_;
-                    break;
-                }
-                token_matches_t match = { fname, token_count };
-                string_deinit(&(best_matches.buffer[swap_pos].filename));
-                best_matches.buffer[swap_pos] = match;
-            }
-        }
+    while (exitcode == EXIT_SUCCESS_ && (dent = readdir(dir))) {
+        exitcode = scan_file(dent->d_name, token, &best_matches, best_of);
     }
     closedir(dir);
 
